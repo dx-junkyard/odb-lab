862746c96f940c95984805bdc5056f0c555f7f5d
fooqoo <horseshoes.buzz.17@gmail.com>
2024-09-08 12:08:09 +0900

    build: open-interepreter

--- backend/server.py
+++ backend/server.py
@@ -2,7 +2,11 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
 from router import interpreter_router
+import uvicorn
 
 app = FastAPI()
 
 app.include_router(interpreter_router.router)
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=58912)
